217e7e132395be572d4887a646a7d42f80b4b364
Neil Sainsbury <neil.sainsbury@foxtel.com.au>
2022-05-09 13:19:36 +1000

    Completed Day 2 (Part B)

---
+++ src/Day02b.kt
@@ -0,0 +1,32 @@
+import java.io.File
+
+object Submarine {
+    var dx = 0
+    var dy = 0
+    var aim = 0
+
+    fun processCommand(commandString: String) {
+        val parts = commandString.split(" ")
+
+        val command = parts[0]
+        val units = parts[1].toInt()
+
+        when (command) {
+            "forward" -> {
+                dx += units
+                dy += (aim * units)
+            }
+            "down" -> aim += units
+            "up" -> aim -= units
+        }
+    }
+}
+
+fun main() {
+    File("data/day02_input.txt")
+        .forEachLine { command ->
+            Submarine.processCommand(command)
+        }
+
+    println(Submarine.dx * Submarine.dy)
+}
